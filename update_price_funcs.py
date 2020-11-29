bd9b66616641f7f2dd54fda3b757ba677d5c5612
beachc15 <60740158+beachc15@users.noreply.github.com>
2020-11-28 18:31:00 -0600

    updated the update_price_funcs.py to now work for the current week by examining the day of the week, finding monday, and running on every file recorded between monday and the current day.

--- update_price_funcs.py
+++ update_price_funcs.py
@@ -1,118 +1,186 @@
-# As stock data does not update on yfinance for 15 minutes after publishing this script
-# will look at our csv upload from 15 minutes ago and update its values accordingly
-import os
 import csv
 import pandas as pd
+from pytz import utc
+import warnings
+import datetime
 
 
-def main(prices_):
-    file_path_check_file = '/home/pi/Documents/data/check_file.csv'
-    file_path_edit = '/home/pi/Documents/data/options_daily/'
-    files_to_fix = []
-    files_to_keep = []
-    # get the list of files to fix
-    # files should be seperated by a line with the format {%m_%d_%y-%H:%M}.csv
-    '''
-    with open(file_path_check_file, 'r') as f:
-        reader = csv.reader(f)
-        for row in reader:
-            files_to_fix.append(row)
-            '''
-    
-    os.chdir("/home/pi/Documents/data/options_daily/")
-    for root, dirs, files in os.walk(".", topdown = False):
-        for name in files:
-            files_to_fix.append(os.path.join(root, name))
-    try:
-        for fn in files_to_fix:
-            #fn = fn.strip()
-            #dir_name = fn.split('-')[0]
-            #file_path = file_path_edit + dir_name + '/' + fn
-            df = pd.read_csv(fn, index_col='uid')
-            check = df.head(1)
-            if check['currentPriceDay'].isnull().sum().sum() != 0:
-                response = get_price_info(df, prices_, fn)
-                if response == 'keep':
-                    files_to_keep.append(fn)
-                else:
-                    pass
-        with open(file_path_check_file, 'w') as f:
-            if isinstance(files_to_keep, list):
-                print(files_to_keep)
-                f.write(', '.join(files_to_keep))
-                # csv.writer(f).writerow(', '.join(files_to_keep))
-            elif isinstance(files_to_keep, str):
-                f.write(files_to_keep)
-                # csv.writer(f).writerow(files_to_keep)
-            else:
-                print("error with writer")
-                print(f"files to keep was {files_to_keep}")
-                print(f"and of type {type(files_to_keep)}")
+def main():
+    """
+    Attempts to update dataframes for previous week by adding additional data such as the pct change needed to reach
+    strike from current stock price, current stock price, current volume, etc.
+    :return:
+    """
 
-    except IndexError:
-        pass
+    def get_dates():
+        """
+        Finds today's date and finds monday of this week. Returns both as datetime objects
+        :return:
+        """
+        # find latest date in directory
+        # take that date (end date), convert to datetime object, check day of week, find monday of that week,
+        # set monday as start time
+        # using CRONTAB this should only be run on fridays but this will work for error handling
+        friday_check = False
+        friday_int = 5
 
-def get_price_info(df_, prices, file_name):
-    from datetime import time
-    from pytz import utc
+        cur_date = datetime.datetime.now(tz=utc).date()
+        year_, week_, weekday_ = cur_date.isocalendar()
 
-    '''
-    def get_yfinance(tickers):
+        # check day of week. If the day is not friday then throw a warning but continue running.
+        if weekday_ != friday_int:
+            if weekday_ > friday_int:
+                # it is a weekend. Set day to Friday
+                weekday_ = friday_int
+                cur_date = datetime.date.fromisocalendar(year=year_, week=week_, day=weekday_)
+                print(cur_date)
+            warnings.warn(
+                "Program not being run on a Friday. Please correct CRONTAB settings.",
+                UserWarning,
+                stacklevel=1
+                )
+
+        else:
+            friday_check = True
+
+        # Monday is known as integer 1 in the isocalendar format
+        this_monday_date = datetime.date.fromisocalendar(year=year_, week=week_, day=1)
+
+        # At this point we have our timeframe with cur_date being the end date and this_monday_date being the start
+        return this_monday_date, cur_date
+
+    def peruse_dir(start_, end_, my_dir_path="/home/pi/Documents/data/options_daily"):
+        """
+        Finds every file within the directories labeled within the start and end date parameters.
+
+        :param my_dir_path: top level directory to walk throught
+        :param end_: datetime.date object of start date to find
+        :param start_: datetime.date object of start date to find
+        :return files_list: List of files written from the top level dir path.
+        """
+        import os
+        # set to parent folder of your data repository where every subfolder is labeled with the date of record
+        files_list = []
+
+        # for loop for each of the dates between start and end
+        year, week, week_no_start = start_.isocalendar()
+        _, _, week_no_end = end_.isocalendar()
+        # below we create the list of dates to run our walk function on
+        date_list = [datetime.date.fromisocalendar(year=year, week=week, day=x).strftime('%m_%d_%y') for x in range(week_no_start, week_no_end + 1)]
+        # ****** Get list of files *******
+        for date in date_list:
+            date_path = f'{my_dir_path}/{date}'
+            os.chdir(date_path)
+            for root, dirs, files in os.walk(".", topdown=False):
+                for name in files:
+                    files_list.append(os.path.join(root, name))
+        # Checked this function on a windows machine. Have yet to check on my actual raspberry pi
+        print('***********************\n\n')
+        print('file names')
+        print(files_list)
+        print('\n\n***********************')
+        # first file should be a directory titled as the start_ date as a string in format(%m_%d_%y)
+        # run a list for all datetime dates in between start and end, convert them to string and see if theres a
+        return files_list
+
+    def get_yfinance(start_, end_):
+        """
+        looks up the prices of stocks and the current volume of those stocks at a 1 minute interval between the start
+        and end date specified in the args.
+        :param start_:
+        :param end_:
+        :return:
+        """
         import yfinance as yf
-        prices = yf.download(tickers=tickers, interval='1m', period='1d')
-        return prices
-    '''
+        # dir_str = '/home/pi/python_projects/python_prod/rasbpi_options/tickers.csv'
+        dir_str = 'tickers.csv'
 
-    def price_delta_in_pct(strike, current_price):
-        """finds the percent change necessary from current price to reach strike price"""
-        pct_change = round((strike - current_price) / current_price, 7)
-        return round(pct_change, 7)
-    '''
-    def get_stock_list():
-        with open('/home/pi/python_projects/python_prod/rasbpi_options/tickers.csv', 'r') as fd:
-            for tickers_ in csv.reader(fd):
-                pass
-        return tickers_
-    '''
+        # Get ticker list
+        with open(dir_str) as f:
+            for tickers_ in csv.reader(f):
 
-    # Instantiate the datetime object we will use to match times later
-    actual_match = 0
-    # stock_list = get_stock_list()
-    # current_prices = get_yfinance(stock_list)
-    #time_as_str = list(map(int, file_name.split('.')[
-     #   0].split('-')[-1].split(':')))
-    time_as_str = list(map(int, (file_name.split('-')[-1].split('.')[0].split(':'))))
-    time_obj = time(hour=time_as_str[0], minute=time_as_str[1])
-    for ind in prices.index:
-        ind_match = ind.astimezone(utc)
-        if ind_match.time() == time_obj:
-            actual_match = ind
-            break
-    try:
-        adj_close_series = prices.loc[actual_match]['Adj Close']
-        volume = prices.loc[actual_match]['Volume'].to_dict()
-        adj_close = adj_close_series.to_dict()
+                # Weirdly this works for instantiating the tickers object. I know its inefficient but meh.
+                print(tickers_)
 
-        df_['currentPriceDay'] = df_['ticker'].apply(
-            lambda x: adj_close.get(x))
-        df_['stockVolumeDay'] = df_['ticker'].apply(lambda x: volume.get(x))
-        df_['pctPriceDiff'] = df_.apply(lambda x: price_delta_in_pct(
-            x['strike'], x['currentPriceDay']), axis=1)
+        # Download data from yfinance
+        df_out = yf.download(tickers=tickers_, start=start_, end=end_, interval='1m')
+        return df_out[['Adj Close', 'Volume']]
 
-        if adj_close_series.isnull().sum().sum() != 0:
-            print('YAYYYYY')
-            return 'keep'
-        elif adj_close_series.isnull().sum().sum() == 0:
-            df_.to_csv(f'/home/pi/Documents/data/options_daily/{file_name}')
-            return 'remove'
-    except KeyError:
-        return 'keep'
+    def alter_csv(price_vol_df, file_str_):
+        """
+        IMPORTANT: This function actually opens and (potentially permanently) alters the file that is passed into it.
+        :param price_vol_df: datetime.date Object
+        :type file_str_: datetime.date Object
+        :return:
+        """
+        # data function
+        def price_delta_in_pct(strike, current_price):
+            """
+            Finds the percent change necessary from current price to reach strike price
+            :param strike:
+            :param current_price:
+            :return:
+            """
+            round_int = 7
+            pct_change = (round((strike - current_price) / current_price), round_int)
+            return pct_change
+
+        def get_time(file_str__):
+            """
+            Get recorded time from the file_str file name
+            :param file_str__:
+            :return:
+            """
+            hour_and_minute = list(map(int, (file_str__.split('-')[-1].split('.')[0].split(':'))))
+            time_obj = datetime.time(hour=hour_and_minute[0], minute=hour_and_minute[1])
+            return time_obj
+
+        confirmed_match = False
+        df_index_col = 'uid'
+
+        df = pd.read_csv(file_str_, index_col=df_index_col)
+        # TODO add some check to see if this dataframe has already been altered (just in case)
+        #   honestly no this is dumb to check. I will leave it in just in case I change my mind but if its there whats
+        #   the harm in just overwriting it?
+
+        # Get recorded time from the file_str file name
+        my_time = get_time(file_str_)
+
+        # Find the matching time from the yfinance csv dataframe that corresponds with the time stored as "my_time"
+        for ind in price_vol_df.index:
+            ind_match = ind.astimezone(utc)
+            if ind_match.time() == my_time:
+                # confirmed_match is stored as a UNIX timestamp (I think)
+                # TODO confirm line 122
+                confirmed_match = ind
+
+        # confirmed_match is the index of the row in the stock prices dataframe which we need to use
+        # I should make the next part a "try.. except" method but I want to see what happens this way first
+
+        if not confirmed_match:
+            raise KeyError(f'confirmed_match did not find a match with time {my_time}')
+        adj_close_dict = price_vol_df.loc[confirmed_match]['Adj Close'].to_dict()
+        volume_dict = price_vol_df.loc[confirmed_match]['Volume'].to_dict()
+
+        # **Transformation**
+        df['currentPriceDay'] = df['ticker'].apply(lambda x: adj_close_dict.get(x))
+        df['stockVolumeDay'] = df['ticker'].apply(lambda x: volume_dict.get(x))
+        df['pctPriceDiff'] = df.apply(lambda x: price_delta_in_pct(x['strike'], x['currentPriceDay']), axis=1)
+
+        df.to_csv(path_or_buf=file_str_)
+
+    start, end = get_dates()
+    file_list = peruse_dir(start_=start, end_=end)
+    df_prices = get_yfinance(start_=start, end_=end)
+
+    for file_str in file_list:
+        alter_csv(df_prices, file_str)
+
+    # at this point we can start checking each file and altering it
+    # As of right now I think I am going to pass it into a for loop for each of the file strings in file_list
+    # and pass a function that actually goes in and makes the required alteration of the CSV.
+    #       I won't be able to bug-check this on my windows machine
 
 
 if __name__ == '__main__':
-    with open('/home/pi/python_projects/python_prod/rasbpi_options/tickers.csv', 'r') as fd:
-        for tickers_ in csv.reader(fd):
-            pass
-    import yfinance as yf
-    df_in = yf.download(tickers=tickers_, interval='1m', period='5d')
-    main(df_in)
+    main()
