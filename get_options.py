79940d08d06e9961a3e6ae3a821b799639adb70a
Beachc15 <beachc15@gmail.com>
2020-12-15 15:44:35 -0600

    set end time to 21:00 UTC (4:00PM EST)

--- get_options.py
+++ get_options.py
@@ -131,7 +131,7 @@ def run_program(export_dir_path='/home/pi/Documents/data/options_daily'):
     current = datetime.datetime.now(tz=utc)
     weekno = datetime.datetime.today().weekday()
     open_time = datetime.time(hour=14, minute=30)
-    close_time = datetime.time(hour=21, minute=30)
+    close_time = datetime.time(hour=21)
     if open_time < current.time() < close_time and weekno < 5:
         str_dt = current.strftime('%m_%d_%y')
 
