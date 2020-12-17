072e83170614da2de461f3efe2d699a99eecab89
Beachc15 <beachc15@gmail.com>
2020-12-17 10:43:26 -0600

    EOD to record for whole week changed EOD script to lookup prices for the whole week (as yahoo stores a week worth of 5-min data) to be run by crontab on fridays. Not sure if this will work in practice, TODO a report on friday after markets close

--- EOD.py
+++ EOD.py
@@ -4,6 +4,7 @@ import datetime
 import yfinance as yf
 import csv
 from pytz import utc
+from update_price_funcs import get_dates
 
 __author__ = "Charles Beach"
 __credits__ = "Charles Beach"
@@ -27,24 +28,26 @@ def end_of_day_collection():
 
     ticker_str = '/home/pi/python_projects/python_prod/rasbpi_options/tickers.csv'
     current = datetime.datetime.now(tz=utc)
-    weekno = datetime.datetime.today().weekday()
-    if (21 <= current.hour < 24) and weekno < 5:
+    # weekno = datetime.datetime.today().weekday()
+    year_, week_, weekday_ = current.isocalendar()
+    if (21 <= current.hour < 24) and weekday_ < 6:
         dt = current.strftime('%Y-%m-%d')
 
         with open(ticker_str) as f:
             for tickers in csv.reader(f):
                 _ = tickers
         # export = {'date': dt, 'data': yf.download(tickers, interval='5m', start=dt).to_json()}
-        open_str = f"/home/pi/Documents/data/EOD_prices/{dt}.csv"
+        open_str = f"/home/pi/Documents/data/EOD_prices/week-{week_}_{year_}.csv"
+        start_, end_ = get_dates()
         with open(open_str, 'w') as f:
-            df = yf.download(tickers, interval='5m', start=dt)
+            df = yf.download(tickers, interval='5m', start=start_, end=end_)
             df = fix_names(df)
             df.to_csv(f)
         print()
         print()
         print('***********************')
         print()
-        print(f'Successfully output to ~/Documents/data/EOD_prices/{dt}.csv')
+        print(f'Successfully output to {open_str}')
         print()
         print('***********************')
         print()
@@ -52,7 +55,7 @@ def end_of_day_collection():
     else:
         print(print(f'Will not run EOD as time is currently: {current}'))
         print('***********************')
-        print(f'weekno: {weekno}')
+        print(f'weekno: {weekday_}')
         print(f'Current Hour: {current.hour}')
         print('***********************')
 
